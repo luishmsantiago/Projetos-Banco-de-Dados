f550af36d00f39a944235b0e6aba8634b9d582a1
Luis Henrique de Melo Santiago <93355437+luishmsantiago@users.noreply.github.com>
2024-07-22 21:04:50 -0300

    Add files via upload

---
+++ mainGPT.sql
@@ -0,0 +1,28 @@
+-- Banco de dado Sistemas Corporais
+
+--Autor: Luis Henrique de Melo Santiago
+--Data Criação: 08/07/2024
+--Última atualização: 22/07/2024
+--Banco de dados com o objetivo de organizar todas as estruturas corporais 
+--e seus relacionamentos.
+
+-- Banco de dados (DML) inserção de itens
+-- O import será visto no futuro.
+
+CREATE TABLE sistemasCorporais;
+
+\i 'tabelas/estruturas.sql'
+\i 'tabelas/estruturasOrgaos.sql'
+\i 'tabelas/executar.sql'
+\i 'tabelas/funcao.sql'
+\i 'tabelas/inervar.sql'
+\i 'tabelas/inervarOrgao.sql'
+\i 'tabelas/inserir.sql'
+\i 'tabelas/musculos.sql'
+\i 'tabelas/nervosPeriSna.sql'
+\i 'tabelas/orgaos.sql'
+\i 'tabelas/ossos.sql'
+\i 'tabelas/sistemaNervosoCentral.sql'
+\i 'tabelas/sncentralPeriSna.sql'
+\i 'tabelas/vascularizar.sql'
+\i 'tabelas/vasos.sql'
